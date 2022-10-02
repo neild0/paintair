5e4a49a063abc65f4c761d94ed581cab091161b5
neil-argus <neil.nitin.de@Gmail.com>
2022-10-01 22:59:49 -0400

    added stable diffusion code to hack

--- main.py
+++ main.py
@@ -114,7 +114,24 @@ while True:
     if k == 27:
         break
 
-    else:
-        print('Waiting')
-
 print(drawing)
+
+from stable_diffusion_tf.stable_diffusion import StableDiffusion
+from PIL import Image
+
+generator = StableDiffusion(
+    img_height=512,
+    img_width=512,
+    jit_compile=False,  # You can try True as well (different performance profile)
+)
+
+img = generator.generate(
+    "a high quality sketch of the sun , watercolor , pencil color",
+    num_steps=50,
+    unconditional_guidance_scale=7.5,
+    temperature=1,
+    batch_size=1,
+    input_image="test4.png",
+    input_image_strength=0.8
+)
+pil_img = Image.fromarray(img[0])
