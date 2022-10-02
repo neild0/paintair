4e490cf471ab0a5127cbd651291225981383cfbc
eshaan moorjani <eshaan@eshaans-MacBook-Pro.local>
2022-10-02 03:42:47 -0400

    sod

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, flash, request, redirect, url_for
 import numpy as np
 import cv2
 import json
+from io import BytesIO
+
+
 
 server = Flask(__name__)
 
@@ -17,29 +20,33 @@ print("server is ready")
 
 @server.route('/sd', methods=['POST'])
 def upload_drawing():
-    if request.method == 'POST':
-        import base64
-        print(request)
-        print(request.data)
-        j = json.loads(request.data)
-        j = j['img']
-        jpg_as_text = base64.b64decode(j)
-        img = cv2.imdecode(jpg_as_text, cv2.IMREAD_COLOR)
-        cv2.imwrite("black.png", img)
-        img = generator.generate(
-            "a high quality sketch of the sun , watercolor , pencil color",
-            num_steps=50,
-            unconditional_guidance_scale=7.5,
-            temperature=1,
-            batch_size=1,
-            input_image="black.png",
-            input_image_strength=0.8
-        )
-        retval, buffer = cv2.imencode('.jpg', img)
-        # Convert to base64 encoding and show start of data
-        jpg_as_text = base64.b64encode(buffer)
-        files = {'img': jpg_as_text}
+    try:
+        if request.method == 'POST':
+            import base64
+            print(request)
+            print(request.data)
+            j = json.loads(request.data)
+            j = j['img']
+            jpg_as_text = base64.b64decode(j)
+            img = BytesIO(jpg_as_text)
+            img = Image.open(img)
+            cv2.imwrite("black.png", img)
+            img = generator.generate(
+                "a high quality sketch of the sun , watercolor , pencil color",
+                num_steps=50,
+                unconditional_guidance_scale=7.5,
+                temperature=1,
+                batch_size=1,
+                input_image="black.png",
+                input_image_strength=0.8
+            )
+            retval, buffer = cv2.imencode('.jpg', img)
+            # Convert to base64 encoding and show start of data
+            jpg_as_text = base64.b64encode(buffer)
+            files = {'img': jpg_as_text}
 
 
-        return {'img': json.dumps(jpg_as_text)}
+            return {'img': json.dumps(jpg_as_text)}
+    except e:
+        print(e)
         # pil_img.save("output.png")
