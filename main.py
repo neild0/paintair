61fcd851d49070dd06514de75ffbd5182c6445d5
eshaan moorjani <eshaan@eshaans-MacBook-Pro.local>
2022-10-01 23:24:17 -0400

    created open palm detection

--- main.py
+++ main.py
@@ -5,9 +5,28 @@ import numpy as np
 import mediapipe as mp
 
 from collections import deque
-import speech_recognition as sr
+# import speech_recognition as sr
+sr = None
 
-camera = cv2.VideoCapture(1)
+def angle_btw_points(point1, point2, base):
+    a = np.array([abs(base[0] - point1[0]), abs(base[1] - point1[1])])
+    b = np.array([abs(base[0] - point2[0]), abs(base[1] - point2[1])])
+
+    inner = np.inner(a, b)
+    norms = np.linalg.norm(a) * np.linalg.norm(b)
+
+    cos = inner / norms
+    rad = np.arccos(np.clip(cos, -1.0, 1.0))
+    deg = np.rad2deg(rad)
+
+    return deg
+
+def dist(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2
+
+camera = cv2.VideoCapture(0)
 cv2.namedWindow("test")
 
 RED = (0,0,255)
@@ -62,14 +81,15 @@ def callback(recognizer, audio):
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+if sr: 
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    with m as source:
+        r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
 
-# start listening in the background (note that we don't have to do this inside a `with` statement)
-r.listen_in_background(m, callback, phrase_time_limit=2)
-# `stop_listening` is now a function that, when called, stops
+    # start listening in the background (note that we don't have to do this inside a `with` statement)
+    r.listen_in_background(m, callback, phrase_time_limit=2)
+    # `stop_listening` is now a function that, when called, stops
 
 while True:
     w, h = 1920//2, 1080//2
@@ -87,14 +107,23 @@ while True:
     results = hands.process(imageRGB)
 
     # checking whether a hand is detected
+    h, w, c = img.shape
     if results.multi_hand_landmarks:
+        index_tip8, index_mcp5, middle_tip12 = None, None, None
         for handLms in results.multi_hand_landmarks: # working with each hand
-            for id, lm in enumerate(handLms.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                if id == 8: # tip of the pointer finger
-                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-                    pts.appendleft((cx, cy))
+            index_tip8 = int(handLms.landmark[8].x * w), int(handLms.landmark[8].y * h)
+            middle_tip12 = int(handLms.landmark[12].x * w), int(handLms.landmark[12].y * h)
+            wrist0 = int(handLms.landmark[0].x * w), int(handLms.landmark[0].y * h)
+            thumb_tip4 = int(handLms.landmark[4].x * w), int(handLms.landmark[4].y * h)
+
+            print("dist is ", dist(index_tip8, middle_tip12))
+            if (angle_btw_points(index_tip8, thumb_tip4, wrist0) <= 30.0
+                and dist(index_tip8, middle_tip12) > 15000):
+                cx, cy = index_tip8
+                cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                pts.appendleft((cx, cy))
+            else:
+                pts.appendleft(None)
     
     if END:
         break
